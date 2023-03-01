9f30eb09be89ba2511451bf3d81591d6c2f66d36
= <=>
2023-03-01 08:00:46 +0000

    Creating a state for

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,24 @@
 import { Component } from 'react';
-
-
 import './App.css';
 import logo from './logo.svg';
 
-class App extends Component() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Tejas"
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Hii {this.state.name}</p>
+          <button>
+            Change Name
+          </button>
         </header>
       </div>
     );
